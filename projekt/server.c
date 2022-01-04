6ad801f9b83577bce163185871e18bdf3c9865f1
Jan Krenz <51825468+JaJasiok@users.noreply.github.com>
2022-01-04 16:54:46 +0100

    ok

--- projekt/server.c
+++ projekt/server.c
@@ -10,6 +10,8 @@
 #include<sys/time.h>
 
 int MAX_length = 50;
+int MAX_users = 5;
+int MAX_rooms = 5
 
 struct history{
     char messages[10][MAX_length];
@@ -21,13 +23,24 @@ struct msgbuf{
     char text[MAX_length];
 }
 
+
+
+
+
+
+
 int main(int argc, char*argv[]){
 
-    char users[5][20];
-    char rooms[5][20];
+    char users[MAX_users][20];
+    char rooms[MAX_rooms][20];
 
-    int users_count = 0;
-    int rooms_count = 0;
+    for(int i = 0; i < MAX_users){
+        memset(users[i],'\0',strlen(users[i]));
+    }
+
+    for(int i = 0; i < MAX_rooms){
+        memset(rooms[i],'\0',strlen(rooms[i]));
+    }
 
     char commands = {"exit", "enter", "logout", "private", "server", "room", "in_room", "on_server", "help", "history"};
 
