cdc5c316798dadfe1f3a4032aed8ae365f371225
Jan Krenz <51825468+JaJasiok@users.noreply.github.com>
2022-01-06 18:57:05 +0100

    nie wiem co

--- projekt/client.c
+++ projekt/client.c
@@ -9,14 +9,49 @@
 #include<sys/msg.h>
 #include<sys/time.h>
 
+int MAX_length = 50;
+
+struct msgbuf{
+    long type;
+    char tab[MAX_length];
+    
+} send, receive;
+
+
+
+void clear(){
+    memset(send,'\0',strlen(send));
+}
+
+
+
+
 int main(int argc, char*argv[]){
 
-    char buf[100];
+    int id = mssget(argv[1], 0644 | IPC_CREAT);
+
+    send.type = 0;
+
+    bool valid = False;
 
     while(1){
-        scanf("%s", &buf);
+        printf("Enter username:");
+        scanf("%s", &send.tab);
+        msgsnd(id, &send, strlen(send.tab)+1, 0);
+        if(valid = True){
+            break;
+        }
+        else{
+            printf("This username is already taken! Please choose another one.")
+        }
     }
+    execlp("clear", "clear", NONE)
     
+    valid = False;
+    
+    printf("Enter room name:");
+    scanf("%s", &send.tab);
+    msgsnd(id, &send, strlen(send.tab)+1, 0);
 
     return 0;
 }
